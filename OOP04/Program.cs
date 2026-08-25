4856a51684e045c3967d7d3d3b83b3a2f4d4d5d5
JanaShebl <janamohamed195200@gmail.com>
2026-08-25 03:12:28 +0300

    Q1

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -8,6 +8,20 @@
             #region old code
 
             #endregion
+
+            #region Q1
+
+            // a) What happens when you assign one object variable to another object variable?
+            // the two variables will refer to the same ref. in the memory (copy ref.) , new object is created
+
+            // b) Does assigning one object to another create a new object? Explain.
+            // No , both of them will be share the same ref. in the memory , if you change one of them , both will be affected
+
+            // c) What is the difference between copying an object and copying its reference?
+            // object => new object created in the memory with the same data
+            // ref. => only copy the ref. in the memory (no new object)
+
+            #endregion
         }
     }
 }
