979c5931ec8ca4ed257e1afd411054e9d2b42c69
JanaShebl <janamohamed195200@gmail.com>
2026-08-25 03:30:23 +0300

    Q2

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -22,6 +22,25 @@
             // ref. => only copy the ref. in the memory (no new object)
 
             #endregion
+
+            #region Q2
+
+            // a) What is a Shallow Copy?
+            // create a new object and copies all value types , nested objects only copies the ref.
+
+            // b) What is a Deep Copy?
+            // create a new object and copies all value types , nested objects (original and copy are independent)
+
+            // c) What happens to reference-type members when a Shallow Copy is created?
+            // they share the same ref. ,if you change one of them both will be affected
+
+            // d) What happens to reference-type members when a Deep Copy is created?
+            // original and copy are independent كل واحدة بتشاور على مكان لو غيرت في واحدة مش هيتأثر التاني
+
+            // e) Give one situation where Deep Copy would be safer than Shallow Copy.
+            // if the business case require an isolation between objects
+
+            #endregion
         }
     }
 }
